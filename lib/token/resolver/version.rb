630f57368f69ff0050281451c610b1b73a19c819
Peter H. Boling <peter.boling@gmail.com>
2026-08-01 20:58:19 -0600

    🔖 Bump gem version

--- lib/token/resolver/version.rb
+++ lib/token/resolver/version.rb
@@ -5,7 +5,7 @@ module Token
     # Version namespace for this gem.
     module Version
       # Current gem version.
-      VERSION = "2.0.7"
+      VERSION = "2.0.8"
     end
     # Current gem version exposed at the traditional constant location.
     VERSION = Version::VERSION # Traditional Constant Location
